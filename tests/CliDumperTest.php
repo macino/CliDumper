c2f9a66b930e53a5940a22e9fef6c5ac3fb74013
tomas.macik <tomas.macik@gmail.com>
2023-12-19 18:40:15 +0100

    CliDumper test: test of complex object

--- tests/CliDumperTest.php
+++ tests/CliDumperTest.php
@@ -20,5 +20,19 @@ class CliDumperTest extends TestCase
     public function testDump(): void
     {
         $this->assertEquals("\n# Test Var: [34m'stringValue'[0m\n", $this->dump('Test Var', 'stringValue') , 'String');
+
+        $testArr = [
+            "string" => "Hello",
+            "integer" => 123,
+            "float" => 3.14,
+            "boolean" => true,
+            "null" => null,
+            "numericArray" => [1, 2, 3],
+            "emptyArray" => [],
+            "object" => new stdClass()
+        ];
+
+        $this->assertEquals("\n# Test Var: \nstring: [34m'Hello'[0m\ninteger: [36m123[0m\nfloat: [36m3.14[0m\nboolean: [35mTRUE[0m\nnull:: [35mNULL[0m\nnumericArray:: 0: [36m1[0m, 1: [36m2[0m, 2: [36m3[0m\nemptyArray:: [32m... @empty[0m\nobject:: \n", $this->dump('Test Var', $testArr) , 'Complex');
+
     }
 }
